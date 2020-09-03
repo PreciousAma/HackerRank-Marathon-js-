662ab2f32623b927fbc507038618ad2e967cc5a2
AmaraCP <preshchilaka06@gmail.com>
2020-09-03 16:37:36 +0100

    string-comparison.js completed, diagonal-difference unfinished

---
+++ diagonal-difference.js
@@ -0,0 +1,6 @@
+const diagonalDifference = (arr) => {
+    let  
+}
+
+const result = diagonalDifference([[1, 2, 3] [4, 5, 6] [9, 8, 9]]);
+console.log({result});
---
+++ string-comparision.js
@@ -0,0 +1,12 @@
+const compareString = (strOne, strTwo) => {
+    const stringOne = strOne.length;
+    const stringTwo = strTwo.length;
+
+    if (stringOne === stringTwo)
+        return true;
+
+      else 
+        return false;  
+}
+
+compareString("james", "janet");
