bffc6f6b63f934210af83334fb12dca5a6a066ee
omobosteven <omobosteven@gmail.com>
2020-09-04 10:34:24 +0100

    create plusMinus file

---
+++ plusMinus.js
@@ -0,0 +1,25 @@
+const plusMinus = (arr) => {
+  // For 6 places after decimal use the toFixed method.
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed#:~:text=Description,it%20has%20the%20specified%20length.
+
+
+
+
+  // Write Your Code Above this line🙄
+  console.log('\n');
+}
+
+plusMinus([0, 0, -1, 1, 1]);
+// 0.400000
+// 0.200000
+// 0.400000
+
+plusMinus([-4, 3, -9, 0, 4, 1]);
+// 0.500000
+// 0.333333
+// 0.166667
+
+plusMinus([1, 2, 3, -1, -2, -3, 0, 0]);
+// 0.375000
+// 0.375000
+// 0.250000
